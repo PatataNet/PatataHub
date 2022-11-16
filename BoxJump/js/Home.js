fec93f7544cf16537cd60cff200881575ce22dc2
Vato <88251332+PatataNet@users.noreply.github.com>
2022-11-16 14:44:26 -0600

    Update Home.js

--- BoxJump/js/Home.js
+++ BoxJump/js/Home.js
@@ -89,6 +89,7 @@
                 case(4): updateUI = window.Game.theme.setTheme("hellLand"); break;
                 case(5): updateUI = window.Game.theme.setTheme("lightUniverse"); break;
                 case(6): updateUI = window.Game.theme.setTheme("voidWorld"); break;
+		case(7): updateUI = window.Game.theme.setTheme("randomLand"); break;
             }
             if (updateUI == true) this.updateUIColor();
             return updateUI;
