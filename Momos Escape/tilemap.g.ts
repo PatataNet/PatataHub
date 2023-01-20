4c6422b217ff5ab973d65d280ac8a3989deb22f6
Vato <88251332+PatataNet@users.noreply.github.com>
2023-01-20 12:18:02 -0600

    Add files via upload

---
+++ Momos Escape/tilemap.g.ts
@@ -0,0 +1,14 @@
+// Código generado automáticamente. No editar.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
