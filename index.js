483c0cf83487ba5a1d031e43af5397306665afca
Vato <88251332+PatataNet@users.noreply.github.com>
2023-02-09 16:19:29 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const letters = "ABCDEFGIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890|@#~€¬!";
+'esversion: 6';
+
+const letters = '/ABCDEFGIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890|@#~€¬!';
 
 let interval = null;
 
-document.querySelector("a").onmouseover = event => {  
+document.querySelector('a').onmouseover = (event) => {
   let iteration = 0;
-  
+
   clearInterval(interval);
-  
+
   interval = setInterval(() => {
     event.target.innerText = event.target.innerText
-      .split("")
+      .split('')
       .map((letter, index) => {
-        if(index < iteration) {
+        if (index < iteration) {
           return event.target.dataset.value[index];
         }
-      
-        return letters[Math.floor(Math.random() * 68)]
+
+        return letters[Math.floor(Math.random() * 68)];
       })
-      .join("");
-    
-    if(iteration >= event.target.dataset.value.length){ 
+      .join('');
+
+    if (iteration >= event.target.dataset.value.length) {
       clearInterval(interval);
     }
-    
-    iteration += 1 / 3;
+
+    iteration += 1 / 6;
   }, 60);
-}
+};
