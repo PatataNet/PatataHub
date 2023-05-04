5aa32b21e818164d70c6b4899078b7baad700cd2
Vato <88251332+PatataNet@users.noreply.github.com>
2023-05-04 13:57:43 -0700

    Create PatataGames.js

---
+++ PatataGames.js
@@ -0,0 +1,17 @@
+const input = document.getElementById('search-input');
+const list = document.getElementById('my-list');
+const items = list.getElementsByTagName('li');
+
+input.addEventListener('keyup', function() {
+  const filter = input.value.toLowerCase();
+
+  for (let i = 0; i < items.length; i++) {
+    const text = items[i].textContent.toLowerCase();
+
+    if (text.includes(filter)) {
+      items[i].classList.add('show');
+    } else {
+      items[i].classList.remove('show');
+    }
+  }
+});
