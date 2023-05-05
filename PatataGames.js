c2b9c7771ae376c6265e105c9dad067cd4ef2638
Vato <88251332+PatataNet@users.noreply.github.com>
2023-05-05 12:01:27 -0700

    Update PatataGames.js

--- PatataGames.js
+++ PatataGames.js
@@ -1,19 +1,22 @@
-const input = document.getElementById('search-input');
-const list = document.getElementById('my-list');
-const items = list.getElementsByClassName('juego');
+document.addEventListener('DOMContentLoaded', function() {
+  const input = document.getElementById('search-input');
+  const list = document.getElementById('my-list');
+  const items = list.getElementsByClassName('juego');
 
-function buscar() {
-  const filter = input.value.toLowerCase();
+  function buscar() {
+    const filter = input.value.toLowerCase();
 
-  for (let i = 0; i < items.length; i++) {
-    const text = items[i];
-    const a2 = text.getElementsById('textoxd');
-    const texto = a2.textContent.toLowerCase();
+    for (let i = 0; i < items.length; i++) {
+      const text = items[i].querySelector('#textoxd');
+      const texto = text.textContent.toLowerCase();
 
-    if (texto.includes(filter)) {
-      items[i].style.display = 'block';
-    } else {
-      items[i].style.display = 'none';
+      if (texto.includes(filter)) {
+        items[i].style.display = 'block';
+      } else {
+        items[i].style.display = 'none';
+      }
     }
   }
-}
+
+  input.addEventListener('input', buscar);
+});
