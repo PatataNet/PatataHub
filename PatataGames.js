1cf74684a9f713746e2267d99edb08232259127c
Vato <88251332+PatataNet@users.noreply.github.com>
2023-05-05 11:18:31 -0700

    Update PatataGames.js

--- PatataGames.js
+++ PatataGames.js
@@ -2,13 +2,15 @@ const input = document.getElementById('search-input');
 const list = document.getElementById('my-list');
 const items = list.getElementsByTagName('li');
 
-input.addEventListener('keyup', function() {
+buscar() {
   const filter = input.value.toLowerCase();
 
   for (let i = 0; i < items.length; i++) {
-    const text = items[i].textContent.toLowerCase();
+    const text = items[i];
+    const a2 = text.getElementsById('textoxd');
+    const texto = a2.textContent.toLowerCase();
 
-    if (text.includes(filter)) {
+    if (texto.includes(filter)) {
       items[i].style.display = 'block';
     } else {
       items[i].style.display = 'none';
