1e05e3e59688ebb882fb8f443165dbe56e9eb99d
Vato <88251332+PatataNet@users.noreply.github.com>
2023-06-14 16:05:28 -0700

    Add files via upload

---
+++ PatataHack/ModoHacker (Broma).bat
@@ -0,0 +1,17 @@
+@echo off
+echo pulsa la tecla F11 + A para iniciar
+pause>nul
+cls
+color a
+dir/s
+dir/s
+dir/s
+dir/s
+dir/s
+dir/s
+dir/s
+tree
+ipconfig
+echo pulsa cualquier tecla para cerrar
+pause>nul
+exit
