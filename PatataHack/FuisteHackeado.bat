88d86b3031f133e0db27eab720d076acfa495de1
Vato <88251332+PatataNet@users.noreply.github.com>
2023-06-14 20:16:46 -0700

    Add files via upload

---
+++ PatataHack/FuisteHackeado.bat
@@ -0,0 +1,11 @@
+@echo off
+start https://www.avast.com/es-mx/index#pc
+start https://www.caliente.mx/
+color a
+netstat
+ipconfig
+start https://www.caliente.mx/
+echo has sido hackeado xdddddd
+pause>nul
+Shutdown -r
+exit
