a19033c70863d0c13e86087887054b80da7d5463
Vato <88251332+PatataNet@users.noreply.github.com>
2023-06-15 17:31:56 -0700

    Add files via upload

---
+++ PatataHack/Hola.bat
@@ -0,0 +1,42 @@
+@echo off
+echo hola
+echo presiona cualquier tecla para continuar
+pause>nul
+cls
+echo como estas?
+echo presiona cualquier tecla para continuar
+pause>nul
+cls
+echo todo bien?
+echo presiona cualquier tecla para continuar
+pause>nul
+cls
+echo bueno solo queria que supieras que te he hackeado >:D
+echo presiona cualquier tecla para continuar
+pause>nul
+cls
+echo XDXDXD
+echo presiona cualquier tecla para continuar
+pause>nul
+cls
+echo Le vendere tus datos a la FBI
+echo presiona cualquier tecla para continuar
+pause>nul
+cls
+echo Incluyendo tu historial
+echo presiona cualquier tecla para continuar
+pause>nul
+cls
+echo aunque los quieras borrar los tendre
+echo presiona cualquier tecla para continuar
+pause>nul
+cls
+echo me hare millonario con todo que tienes ahi bro
+echo presiona cualquier tecla para continuar
+pause>nul
+cls
+echo bueno adios
+echo presiona cualquier tecla para continuar
+pause>nul
+cls
+shutdown -s
